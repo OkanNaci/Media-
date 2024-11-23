66500a880cafb3d94220ec8c0bf16e58ac1cb21c
okan <okan28001@gmail.com>
2024-11-23 17:28:58 +0300

    Create async thunks and fetch user data then implement userSlice with extraReducers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import UsersList from "./components/UsersList";
 function App() {
-  return "app";
+  return (
+    <div className="container mx-auto ">
+      <UsersList />
+    </div>
+  );
 }
 
 export default App;
